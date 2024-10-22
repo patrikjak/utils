5ac5e94e6aa9b0853aa718fdf2e065726daa2fd4
Patrik Jakab <patrikjak@gmail.com>
2024-10-22 22:14:56 +0200

    add example recaptcha site key

--- resources/assets/js/constants.ts
+++ resources/assets/js/constants.ts
@@ -1,3 +1,6 @@
 // ANIMATIONS
 export const DEFAULT_ENTER_ANIMATION = 'animation-slide-in-fwd-center';
 export const DEFAULT_EXIT_ANIMATION = 'animation-slide-out-bck-center';
+
+// RECAPTCHA
+export const RECAPTCHA_SITE_KEY = 'example-site-key';
