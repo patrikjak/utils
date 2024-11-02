6cbc9c09974df068fc971c6b063bd7eec4fe444e
Patrik Jakab <patrikjak@gmail.com>
2024-11-02 19:36:51 +0100

    icons

--- src/UtilsServiceProvider.php
+++ src/UtilsServiceProvider.php
@@ -20,6 +20,7 @@ class UtilsServiceProvider extends ServiceProvider
             $this->publishes(
                 [
                     __DIR__ . '/../resources/assets/css' => resource_path('css/vendor/pjutils'),
+                    __DIR__ . '/../resources/assets/images' => resource_path('images/vendor/pjutils'),
                     __DIR__ . '/../resources/assets/js' => resource_path('js/vendor/pjutils'),
                 ],
                 'assets',
