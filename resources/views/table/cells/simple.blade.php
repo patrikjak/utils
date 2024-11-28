b350692e18f916b34c58ebe8a3e089ff411207d5
Patrik Jakab <patrikjak@gmail.com>
2024-11-28 22:02:20 +0100

    add classes to identify icons

--- resources/views/table/cells/simple.blade.php
+++ resources/views/table/cells/simple.blade.php
@@ -1,6 +1,6 @@
 <td class="{{ $cellClass }}">
     @if($icon !== null)
-        <div class="icon-data">
+        <div class="icon-data {{ $icon }}-icon">
             {!! $icon->getAsHtml() !!}
     @endif
 
