f6e59259b153789a6c51b616ee050b7ba79a9303
Patrik Jakab <patrikjak@gmail.com>
2024-11-30 22:30:31 +0100

    phpcs

--- src/Common/Traits/EnumValues.php
+++ src/Common/Traits/EnumValues.php
@@ -1,11 +1,16 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Patrikjak\Utils\Common\Traits;
 
 use ReflectionClass;
 
 trait EnumValues
 {
+    /**
+     * @return array<string, static>
+     */
     public static function getAll(): array
     {
         $reflection = new ReflectionClass(static::class);
@@ -13,11 +18,17 @@ trait EnumValues
         return $reflection->getConstants();
     }
 
+    /**
+     * @return array<string>
+     */
     public static function getKeys(): array
     {
         return array_column(self::cases(), 'name');
     }
 
+    /**
+     * @return array<string>
+     */
     public static function getValues(): array
     {
         return array_column(self::cases(), 'value');
