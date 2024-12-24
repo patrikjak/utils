17e54468b348e220fccba2d5f413e80ae93d605c
Patrik Jakab <patrikjak@gmail.com>
2024-12-24 17:01:24 +0100

    feat: email layout (#15)
    * email layout
    
    * add codecov badge
    
    * generate coverage

--- config/pjutils.php
+++ config/pjutils.php
@@ -4,4 +4,14 @@ return [
 
     'recaptcha_secret_key' => env('RECAPTCHA_SECRET_KEY'),
 
+    'app_name' => env('APP_NAME', 'App'),
+
+    'app_url' => env('APP_URL', 'http://localhost'),
+
+    /**
+     * Path to the logo - relative to the public directory
+     * Set to null to disable the logo
+     */
+    'email_logo_path' => 'images/logo/logo.svg',
+
 ];
