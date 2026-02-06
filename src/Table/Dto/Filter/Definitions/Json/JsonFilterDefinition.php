d255f291aa44dedcf54ba1656ae5f825ccaa7372
Patrik Jakab <patrik.jakab@team.blue>
2026-02-06 15:41:37 +0100

    Revert "Revert "feat: add JSON filter support to table options""
    This reverts commit 64c6f921

---
+++ src/Table/Dto/Filter/Definitions/Json/JsonFilterDefinition.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Patrikjak\Utils\Table\Dto\Filter\Definitions\Json;
+
+use Patrikjak\Utils\Common\Enums\Filter\FilterType;
+use Patrikjak\Utils\Table\Dto\Filter\Definitions\FilterDefinition;
+
+final readonly class JsonFilterDefinition implements FilterDefinition
+{
+    public function __construct(public ?string $jsonPath = null)
+    {
+    }
+
+    public function getType(): FilterType
+    {
+        return FilterType::JSON;
+    }
+}
