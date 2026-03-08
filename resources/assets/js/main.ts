910aa95ae2d3bc37fcd5052e29a81aeff1dac163
Patrik Jakab <patrikjak@gmail.com>
2026-03-08 13:45:14 +0100

    feat: add modal, tabs, accordions, and clipboard bindings

--- resources/assets/js/main.ts
+++ resources/assets/js/main.ts
@@ -1,24 +1,42 @@
 import Form from './form/Form';
 import notify from './utils/notification';
+import Modal from './utils/Modal';
 import {getData} from './helpers/general';
+import {doAction} from './table/actions';
 import {bindPasswordVisibilitySwitch} from './form/helper';
 import {bindTableFunctions} from './table/table';
 import {bindDropdowns} from './utils/dropdown';
 import {bindUploaders} from './utils/file-uploader';
+import {bindAccordions} from './components/accordion';
+import {bindTabs} from './components/tabs';
+import {bindClipboard} from './components/clipboard';
+import {bindNumberInputs} from './form/number';
+import {bindComboboxes} from './form/combobox';
+import {bindTagsInputs} from './form/tags';
+import {bindRepeaters} from './form/repeater';
 
 declare global {
     interface Window {
-        PjUtils: {
+        pjutils: {
             Form: typeof Form;
             notify: typeof notify;
+            Modal: typeof Modal;
             getData: typeof getData;
+            doAction: typeof doAction;
         };
     }
 }
 
-window.PjUtils = {Form, notify, getData};
+window.pjutils = {Form, notify, Modal, getData, doAction};
 
 bindPasswordVisibilitySwitch();
 bindTableFunctions();
 bindDropdowns();
 bindUploaders();
+bindAccordions();
+bindTabs();
+bindClipboard();
+bindNumberInputs();
+bindComboboxes();
+bindTagsInputs();
+bindRepeaters();
