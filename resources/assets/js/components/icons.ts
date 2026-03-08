9f8dc283b359ade7d71b7231c51d797bbf1c18c5
Patrik Jakab <patrikjak@gmail.com>
2026-03-08 22:04:18 +0100

    fix: update dangerSign SVG for improved icon representation

--- resources/assets/js/components/icons.ts
+++ resources/assets/js/components/icons.ts
@@ -11,7 +11,7 @@ export function successTick(): string {
 }
 
 export function dangerSign(): string {
-    return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>`;
+    return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>`;
 }
 
 export function warningSign(): string {
