84374eb5d9738b6bd38223729c655025b47dbfa8
Patrik Jakab <patrikjak@gmail.com>
2026-03-09 19:45:29 +0100

    chore: update TypeScript configuration and improve i18n setup

--- resources/assets/js/translator.ts
+++ resources/assets/js/translator.ts
@@ -1,12 +1,10 @@
 import {I18n} from "i18n-js";
+import en from './lang/en.json';
+import sk from './lang/sk.json';
 
 const locale = document.documentElement.lang;
-const translations = await import(`./lang/${locale}.json`);
-const i18nTranslations: Record<string, object> = {};
 
-i18nTranslations[locale] = translations.default;
-
-const i18n = new I18n(i18nTranslations);
+const i18n = new I18n({en, sk});
 
 i18n.locale = locale;
 i18n.defaultLocale = 'sk';
